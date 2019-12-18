991f817bc1f15146c9eb601dd811ac5968a7bef2
CaptainBedpan <mr.erik.sharp@gmail.com>
2019-12-18 21:54:42 +0000

    Organizing

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { chapter2 } from "./get-started/chapter-2/chapter-2.js";
+
+chapter2();
+
+let headerMessage = document.getElementById("message");
+headerMessage.innerText = "All of the assertions have run";
